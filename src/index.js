2646b2d79addce18d0d7a4ff448b986d355a8550
aleksei.bulgak <a.bulgak@intetics.com>
2018-09-12 23:32:44 +0300

    implementation that calculates number of trailing zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var countOfPowerOf5Elements = 0,
+    divider = 1,
+    result = 0;
+  do {
+    divider *= 5;
+    result += countOfPowerOf5Elements = Math.floor(number / divider);
+  } while (countOfPowerOf5Elements != 0);
+  return result;
 }
